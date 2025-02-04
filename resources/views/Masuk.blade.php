c43201e10c28ec0d2627c6db19dfaad8412be252
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-02-04 18:12:59 +0700

    Ganti penulisan judul

--- resources/views/Masuk.blade.php
+++ resources/views/Masuk.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rancangan Perkasa - Masuk</title>
+    <title>Rancangan Perkasa | Masuk</title>
     <link rel="icon" href="Images/logo simbol.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
