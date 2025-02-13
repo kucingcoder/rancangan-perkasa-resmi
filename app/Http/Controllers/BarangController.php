b00c85b30ae5b02b885456d4023b8683d9d0f5b7
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-02-13 23:02:30 +0700

    Perubahan sistem deskripsi dan penghapusan

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Barang;
 use App\Models\Kategori;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\Drivers\Gd\Encoders\WebpEncoder;
 
 class BarangController extends Controller
 {
     public function index()
     {
-        $barang = Barang::with('kategori')->get();
+        $barang = Barang::with('kategori')->where('status', 'aktif')->orderBy('nama_barang', 'asc')->get();
+
         $kategori = Kategori::all();
 
         $data = [
@@ -29,24 +34,42 @@ class BarangController extends Controller
             'kategori' => 'required|integer',
             'harga' => 'required|numeric|min:0',
             'stok' => 'required|integer|min:0',
-            'foto' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'foto' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'deskripsi' => 'required|string',
         ]);
+
+        $cek_barang = Barang::where('nama_barang', $request->input('nama_barang'))->where('status', 'aktif')->first();
+
+        if ($cek_barang) {
+            return back()->withErrors('Barang ' . $request->input('nama_barang') . ' sudah ada');
+        }
 
         $barang = new Barang();
         $barang->nama_barang = $request->input('nama_barang');
         $barang->kategori_id = $request->input('kategori');
         $barang->harga = $request->input('harga');
         $barang->stok = $request->input('stok');
+        $barang->deskripsi = $request->input('deskripsi');
 
         if ($request->hasFile('foto')) {
-            $lokasi_full = $request->file('foto')->store('uploads/foto_barang', 'public');
-            $barang->foto = basename($lokasi_full);
+            $image = $request->file('foto');
+            $filename = md5(now());
+            $lokasi_full = 'uploads/foto_barang/' . $filename . '.webp';
+
+            $manager = new ImageManager(new Driver());
+            $imageWebp = $manager->read($image->getPathname())->encode(new WebpEncoder());
+            Storage::disk('public')->put($lokasi_full, $imageWebp);
+
+            $barang->foto = $filename;
+        } else {
+            return back()->withErrors('Periksa kembali data anda');
         }
 
         try {
             $barang->save();
-            return back();
+            return back()->with('success', 'Barang ' . $barang->nama_barang . ' tersimpan');
         } catch (\Throwable $th) {
+            Log::error('KategoriController : ' . $th);
             return back()->withErrors('Periksa kembali data anda');
         }
     }
@@ -71,21 +94,27 @@ class BarangController extends Controller
         $barang->kategori_id = $request->input('kategori_edit');
         $barang->harga = $request->input('harga_edit');
         $barang->stok = $request->input('stok_edit');
+        $barang->deskripsi = $request->input('deskripsi_edit');
 
         if ($request->hasFile('foto_edit')) {
-            if ($barang->foto) {
-                try {
-                    Storage::disk('public')->delete('uploads/foto_barang/' . $barang->foto);
-                } catch (\Throwable $th) {
-                }
+            try {
+                $image = $request->file('foto_edit');
+                $filename = md5(now());
+                $lokasi_full = 'uploads/foto_barang/' . $filename . '.webp';
+
+                $manager = new ImageManager(new Driver());
+                $imageWebp = $manager->read($image->getPathname())->encode(new WebpEncoder());
+                Storage::disk('public')->put($lokasi_full, $imageWebp);
+                Storage::disk('public')->delete('uploads/foto_barang/' . $barang->foto . '.webp');
+                $barang->foto = $filename;
+            } catch (\Throwable $th) {
+                Log::error('BarangController : ' . $th);
             }
-            $lokasi_full = $request->file('foto_edit')->store('uploads/foto_barang', 'public');
-            $barang->foto = basename($lokasi_full);
         }
 
         try {
             $barang->save();
-            return back();
+            return back()->with('success', 'Barang ' . $barang->nama_barang . ' diubah');
         } catch (\Throwable $th) {
             return back()->withErrors('Periksa kembali data anda');
         }
@@ -99,13 +128,14 @@ class BarangController extends Controller
             return redirect()->back()->withErrors('Barang tidak ditemukan.');
         }
 
-        if ($barang->foto) {
-            Storage::disk('public')->delete('uploads/foto_barang/' . $barang->foto);
-        }
+        $barang->status = 'nonaktif';
+        $barang->updated_at = now();
 
-        if ($barang->delete()) {
-            return back();
-        } else {
+        try {
+            $barang->save();
+            return back()->with('success', 'Barang ' . $barang->nama_barang . ' dihapus');
+        } catch (\Throwable $th) {
+            Log::error('BarangController : ' . $th);
             return back()->withErrors('Periksa kembali data anda');
         }
     }
--- resources/views/Barang.blade.php
+++ resources/views/Barang.blade.php
@@ -19,11 +19,28 @@
 </div>
 @endif
 
+@if (session('success'))
+<div id="alert" class="bg-green-300 border border-green-300 text-green-dark px-12 py-3 rounded fixed top-0 right-0 m-4 z-50" role="alert">
+    <strong class="font-bold">Berhasil!</strong>
+    <span class="block sm:inline">{{ session('success') }}</span>
+    <span class="absolute top-0 right-0 px-1 py-3" onclick="tutup()">
+        <svg
+            class="fill-current h-6 w-6 text-red cursor-pointer"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20">
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+        </svg>
+    </span>
+</div>
+@endif
+
 <h2 class="text-2xl md:text-4xl text-center font-bold text-gray-700 mb-2">Daftar Barang</h2>
 
 <!-- Modal Dialog tambah data -->
 <div id="dataModal" class="hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-    <div class="bg-white w-96 p-6 rounded-lg shadow-lg">
+    <div class="bg-white w-3/6 p-6 rounded-lg shadow-lg">
         <!-- Header -->
         <div class="flex justify-between items-center mb-4">
             <h2 id="modalTitle" class="text-lg font-bold text-gray-700">Tambah Data Barang</h2>
@@ -35,34 +52,47 @@
         <!-- Form -->
         <form id="tambahBarang" action="/barang-tambah" method="POST" enctype="multipart/form-data">
             @csrf
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="nama_barang">Nama Barang</label>
-                <input type="text" id="nama_barang" name="nama_barang" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukkan nama barang" required>
-            </div>
+            <div class="flex">
+                <div class="flex flex-col">
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="nama_barang">Nama Barang</label>
+                        <input type="text" id="nama_barang" name="nama_barang" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukan nama barang" required>
+                    </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="kategori">Kategori</label>
-                <select id="kategori" name="kategori" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>
-                    <option value="" disabled selected>Pilih kategori</option>
-                    @foreach ($kategori as $item)
-                    <option value="{{ $item->id }}">{{ $item->nama_kategori }}</option>
-                    @endforeach
-                </select>
-            </div>
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="kategori">Kategori</label>
+                        <select id="kategori" name="kategori" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                            <option value="" disabled selected>Pilih kategori</option>
+                            @foreach ($kategori as $item)
+                            <option value="{{ $item->id }}">{{ $item->nama_kategori }}</option>
+                            @endforeach
+                        </select>
+                    </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="harga">Harga</label>
-                <input type="number" id="harga" name="harga" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukkan harga" required>
-            </div>
+                    <div class="flex">
+                        <div class="mb-4">
+                            <label class="block text-sm font-medium text-gray-700 mb-1" for="harga">Harga</label>
+                            <input type="number" id="harga" name="harga" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukan harga" required>
+                        </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="stok">Stok</label>
-                <input type="number" id="stok" name="stok" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukkan jumlah stok" required>
-            </div>
+                        <div class="ml-4 mb-4">
+                            <label class="block text-sm font-medium text-gray-700 mb-1" for="stok">Stok</label>
+                            <input type="number" id="stok" name="stok" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukan jumlah stok" required>
+                        </div>
+                    </div>
+                </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="foto">Foto (PNG, JPG, JPEG)</label>
-                <input type="file" id="foto" name="foto" accept=".png, .jpg, .jpeg" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                <div class="ml-4 flex flex-col">
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="foto">Foto (PNG, JPG, JPEG)</label>
+                        <input type="file" id="foto" name="foto" accept=".png, .jpg, .jpeg" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                    </div>
+
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="deskripsi">Deskripsi</label>
+                        <textarea name="deskripsi" id="deskripsi" cols="30" rows="4" style="resize: none;" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" id="spesifikasi"></textarea>
+                    </div>
+                </div>
             </div>
 
             <!-- Action Buttons -->
@@ -80,7 +110,7 @@
 
 <!-- Modal Dialog edit data -->
 <div id="dataModaledit" class="hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-    <div class="bg-white w-96 p-6 rounded-lg shadow-lg">
+    <div class="bg-white w-3/6 p-6 rounded-lg shadow-lg">
         <!-- Header -->
         <div class="flex justify-between items-center mb-4">
             <h2 id="modalTitle" class="text-lg font-bold text-gray-700">Edit Data</h2>
@@ -96,33 +126,47 @@
             @csrf
             <input type="hidden" name="id_edit" id="id_edit" required>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="nama_barang_edit">Nama Barang</label>
-                <input type="text" id="nama_barang_edit" name="nama_barang_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukkan nama barang" required>
-            </div>
+            <div class="flex">
+                <div class="flex flex-col">
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="nama_barang_edit">Nama Barang</label>
+                        <input type="text" id="nama_barang_edit" name="nama_barang_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukan nama barang" required>
+                    </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="kategori_edit">Kategori</label>
-                <select id="kategori_edit" name="kategori_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>
-                    @foreach ($kategori as $item)
-                    <option value="{{ $item->id }}">{{ $item->nama_kategori }}</option>
-                    @endforeach
-                </select>
-            </div>
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="kategori_edit">Kategori</label>
+                        <select id="kategori_edit" name="kategori_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                            <option value="" disabled selected>Pilih kategori</option>
+                            @foreach ($kategori as $item)
+                            <option value="{{ $item->id }}">{{ $item->nama_kategori }}</option>
+                            @endforeach
+                        </select>
+                    </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="harga_edit">Harga</label>
-                <input type="number" id="harga_edit" name="harga_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukkan harga" required>
-            </div>
+                    <div class="flex">
+                        <div class="mb-4">
+                            <label class="block text-sm font-medium text-gray-700 mb-1" for="harga_edit">Harga</label>
+                            <input type="number" id="harga_edit" name="harga_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukan harga" required>
+                        </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="stok_edit">Stok</label>
-                <input type="number" id="stok_edit" name="stok_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukkan stok" required>
-            </div>
+                        <div class="ml-4 mb-4">
+                            <label class="block text-sm font-medium text-gray-700 mb-1" for="stok_edit">Stok</label>
+                            <input type="number" id="stok_edit" name="stok_edit" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" placeholder="Masukan jumlah stok" required>
+                        </div>
+                    </div>
+                </div>
 
-            <div class="mb-4">
-                <label class="block text-sm font-medium text-gray-700 mb-1" for="foto_edit">Foto</label>
-                <input type="file" id="foto_edit" name="foto_edit" accept="image/png, image/jpeg, image/jpg" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>
+                <div class="ml-4 flex flex-col">
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="foto_edit">Foto (PNG, JPG, JPEG)</label>
+                        <input type="file" id="foto_edit" name="foto_edit" accept=".png, .jpg, .jpeg" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400">
+                    </div>
+
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700 mb-1" for="deskripsi_edit">Deskripsi</label>
+                        <textarea name="deskripsi_edit" id="deskripsi_edit" cols="30" rows="4" style="resize: none;" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" id="spesifikasi"></textarea>
+                    </div>
+                </div>
             </div>
 
             <!-- Action Buttons -->
@@ -131,7 +175,7 @@
                     Batal
                 </button>
                 <button type="submit" class="mx-2 px-4 py-2 text-sm text-white bg-yellow-500 rounded hover:bg-yellow-600">
-                    Perbarui
+                    Perbaharui
                 </button>
             </div>
         </form>
@@ -193,6 +237,8 @@
                 <th class="bg-gray-200 border-gray-800 px-4 py-2 text-sm text-gray-600">Kategori</th>
                 <th class="bg-gray-200 border-gray-800 px-4 py-2 text-sm text-gray-600">Harga</th>
                 <th class="bg-gray-200 border-gray-800 px-4 py-2 text-sm text-gray-600">Stok</th>
+                <th class="bg-gray-200 border-gray-800 px-4 py-2 text-sm text-gray-600">Spesifikasi</th>
+                <th class="bg-gray-200 border-gray-800 px-4 py-2 text-sm text-gray-600">Deskripsi</th>
                 <th class="bg-gray-200 border-gray-800 px-4 py-2 text-sm text-gray-600">Foto</th>
                 <th class="bg-gray-200 border-gray-800 px-4 py-2 text-sm text-gray-600">Aksi</th>
             </tr>
@@ -207,10 +253,16 @@
                 <td class="border-gray-200 px-4 py-2 text-sm text-gray-700">{{ "Rp. " . number_format($item->harga, 0, ',', '.') }}</td>
                 <td class="border-gray-200 px-4 py-2 text-sm text-gray-700">{{ $item->stok }} {{ $item->kategori->satuan }}</td>
                 <td class="border-gray-200 px-4 py-2 text-sm text-gray-700">
-                    <img src=" {{ asset('storage/uploads/foto_barang/' . $item->foto) }}" alt="Foto {{ $item->nama_barang }}" class="h-16 w-16 object-cover rounded">
+                    <pre>{{ $item->kategori->spesifikasi }}</pre>
                 </td>
                 <td class="border-gray-200 px-4 py-2 text-sm text-gray-700">
-                    <button onclick="edit('{{ $item->id }}','{{ $item->nama_barang }}','{{ $item->kategori_id }}','{{ $item->harga }}','{{ $item->stok }}')" class="px-3 py-1 text-sm text-white bg-yellow-500 rounded hover:bg-yellow-600">Edit</button>
+                    <pre>{{ $item->deskripsi }}</pre>
+                </td>
+                <td class="border-gray-200 px-4 py-2 text-sm text-gray-700">
+                    <img style="cursor: pointer" onclick="window.open(`{{ asset('storage/uploads/foto_barang/' . $item->foto . '.webp') }}`, '_blank');" src="{{ asset('storage/uploads/foto_barang/' . $item->foto . '.webp') }}" alt="Foto {{ $item->nama_barang }}" class="h-16 w-16 object-cover rounded">
+                </td>
+                <td class="border-gray-200 px-4 py-2 text-sm text-gray-700">
+                    <button onclick="edit('{{ $item->id }}','{{ $item->nama_barang }}','{{ $item->kategori_id }}','{{ $item->harga }}','{{ $item->stok }}', `{{ $item->deskripsi }}`)" class="px-3 py-1 text-sm text-white bg-yellow-500 rounded hover:bg-yellow-600">Edit</button>
                     <button onclick="hapus('{{ $item->id }}')" class="px-3 py-1 text-sm text-white bg-red-500 rounded hover:bg-red-600">Hapus</button>
                 </td>
             </tr>
@@ -225,21 +277,16 @@
         const table = new simpleDatatables.DataTable("#daftar-barang");
     });
 
-    function edit(id, nama_barang, kategori, harga, stok, foto) {
+    function edit(id, nama_barang, kategori, harga, stok, deskripsi) {
         document.getElementById('daftar-barang').classList.add('hidden');
         document.getElementById('dataModaledit').classList.remove('hidden');
 
-        // Isi data ke input form edit
         document.getElementById('id_edit').value = id;
         document.getElementById('nama_barang_edit').value = nama_barang;
         document.getElementById('kategori_edit').value = kategori;
         document.getElementById('harga_edit').value = harga;
         document.getElementById('stok_edit').value = stok;
-
-        // Menampilkan foto barang di form edit
-        const fotoPreview = document.getElementById('foto_preview_edit');
-        fotoPreview.src = foto;
-        fotoPreview.alt = `Foto ${nama_barang}`;
+        document.getElementById('deskripsi_edit').value = deskripsi;
     }
 
     function tambah() {
