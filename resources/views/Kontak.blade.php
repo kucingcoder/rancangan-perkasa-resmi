ef1eef1b8fe473404847fb05eae05564bea06a80
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-03-02 09:55:07 +0700

    Buat kontak

--- resources/views/Kontak.blade.php
+++ resources/views/Kontak.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
+@extends('layouts.Landing')
+@section('title', 'Rancangan Perkasa | Kontak')
+
+@section('content')
+<div class="h-screen flex justify-center bg-cover bg-center" style="background-image: url('/Images/photo/kontak.webp');">
+    <div class="mt-72 md:mt-40 flex flex-col mx-8 md:mx-72">
+        <h1 class="mb-6 text-2xl md:text-5xl luxurious-script-regular text-blue-950 drop-shadow-md text-center">
+            RANCANGAN PERKASA
+        </h1>
+        <p class="mb-2 text-xl md:text-3xl luxurious-script-regular text-blue-950 drop-shadow-md text-justify">Jl. Sultan Agung No. 132 RT.006/ RW. 002 Kelurahan Kejambon, Kecamatan Tegal Timur, Kota Tegal.</p>
+        <p class="mb-2 text-xl md:text-3xl luxurious-script-regular text-blue-950 drop-shadow-md text-center">087878786969</p>
+        <p class="text-xl md:text-3xl luxurious-script-regular text-blue-950 drop-shadow-md text-center">rancanganperkasa@gmail.com</p>
+    </div>
 </div>
+@endsection
