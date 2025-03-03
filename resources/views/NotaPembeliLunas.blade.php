22d5787773ba797cf4c9805b7322baf7853b1701
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-03-03 10:04:22 +0700

    Perubahan desain nota & laporan

--- resources/views/NotaPembeliLunas.blade.php
+++ resources/views/NotaPembeliLunas.blade.php
@@ -42,15 +42,17 @@
 </head>
 
 <body>
+    <p style="text-align: right;">{{ $pesanan->kode_invoice }}</p>
+    <br>
     <div style="width: 100%; text-align: center; white-space: nowrap;">
         <div style="display: inline-block; vertical-align: middle; width: 60px;">
             <img src="{{ base_path('/public/Images/logo simbol.png') }}" alt="Rancangan Perkasa" style="width: 60px;">
         </div>
         <div style="display: inline-block; vertical-align: middle; text-align: left; padding-left: 10px;">
-            <h1 style="font-size: 14px; font-weight: bold; margin: 0; text-align: center;">RANCANGAN PERKASA</h1>
-            <h2 style="font-size: 12px; font-weight: bold; margin: 0; text-align: center;">Distribusi Jasa & Bahan Interior Eksterior</h2>
-            <p style="font-size: 10px; margin: 0; text-align: center;">Jl. Sultan Agung No. 132 RT. 006/RW. 002 Desa Kejambon</p>
-            <p style="font-size: 10px; margin: 0; text-align: center;">Kecamatan Tegal Timur, Kota Tegal, Jawa Tengah, Indonesia</p>
+            <h1 style="font-size: 14px; font-weight: bold; margin: 0; text-align: center;">CV. RANCANGAN PERKASA</h1>
+            <h2 style="font-size: 12px; font-weight: bold; margin: 0; text-align: center;">Distributor Jasa & Bahan Interior Eksterior</h2>
+            <p style="font-size: 10px; margin: 0; text-align: center;">Jl. Sultan Agung No. 132 RT.006/RW. 002 Kelurahan Kejambon</p>
+            <p style="font-size: 10px; margin: 0; text-align: center;">Kecamatan Tegal Timur, Kota Tegal, Jawa Tengah, Indonesia</p>
         </div>
     </div>
 
@@ -60,7 +62,6 @@
                 <p style="margin: 0;">{{ $pembeli->nama }}</p>
                 <p style="margin: 0;">{{ $pembeli->no_wa }}</p>
                 <p style="margin: 0;">{{ $pembeli->alamat }}</p>
-                <p style="margin: 0;">Invoice Code : {{ $pesanan->kode_invoice }}</p>
             </td>
             <td style="border: none;"></td>
             <td style="width: 50%; text-align: right; vertical-align: top; padding: 0; border: none;">
