70b1d561859dc967e46696916f15a921e907fad5
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-03-03 08:14:27 +0700

    Update database

--- rancangan_perkasa.sql
+++ rancangan_perkasa.sql
@@ -1,162 +1,461 @@
-CREATE TABLE `kategori` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `nama_kategori` varchar(128) UNIQUE NOT NULL,
-  `ukuran` int NOT NULL,
-  `satuan` varchar(16) NOT NULL,
-  `biaya_sales` int NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Mar 2025 pada 07.42
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
-CREATE TABLE `biaya_pengiriman` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `wilayah` varchar(128) UNIQUE NOT NULL,
-  `nominal` int NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `pembeli` (
-  `id` char(36) PRIMARY KEY,
-  `nama` varchar(128) NOT NULL,
-  `alamat` text NOT NULL,
-  `no_wa` varchar(15) UNIQUE NOT NULL,
-  `email` varchar(364) UNIQUE,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rancangan_perkasa_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akun`
+--
 
 CREATE TABLE `akun` (
-  `id` char(36) PRIMARY KEY,
-  `email` varchar(364) UNIQUE NOT NULL,
-  `no_wa` varchar(15) UNIQUE NOT NULL,
+  `id` char(36) NOT NULL,
+  `email` varchar(364) NOT NULL,
+  `no_wa` varchar(15) NOT NULL,
   `password` char(32) NOT NULL,
   `nama` varchar(128) NOT NULL,
   `foto` varchar(45) NOT NULL,
-  `jenis_kelamin` ENUM ('laki-laki', 'perempuan') NOT NULL,
+  `jenis_kelamin` enum('laki-laki','perempuan') NOT NULL,
   `alamat` text NOT NULL,
-  `jenis_akun` ENUM ('owner', 'admin', 'sales') NOT NULL DEFAULT 'sales',
-  `status` ENUM ('aktif', 'non aktif') NOT NULL DEFAULT 'aktif',
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+  `jenis_akun` enum('sales','admin','owner') NOT NULL DEFAULT 'sales',
+  `status` enum('aktif','nonaktif') NOT NULL DEFAULT 'aktif',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `barang` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `nama_barang` varchar(128) NOT NULL,
-  `kategori_id` int NOT NULL,
-  `harga` int NOT NULL,
-  `stok` int NOT NULL,
-  `foto` char(45) UNIQUE NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+--
+-- Struktur dari tabel `biaya_kirim`
+--
 
-CREATE TABLE `daftar_barang` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `barang_id` int NOT NULL,
-  `keranjang_id` int NOT NULL,
-  `jumlah` int NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE `biaya_kirim` (
+  `id` int(11) NOT NULL,
+  `wilayah` varchar(128) NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `status` enum('aktif','nonaktif') NOT NULL DEFAULT 'aktif',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `keranjang` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `judul` varchar(128) NOT NULL,
-  `akun_id` char(36) NOT NULL,
-  `pembeli_id` char(36) NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_karyawan`
+--
+
+CREATE TABLE `daftar_karyawan` (
+  `id` int(11) NOT NULL,
+  `karyawan_id` int(11) NOT NULL,
+  `lembur_id` int(11) NOT NULL,
+  `uang_lembur` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_produk`
+--
+
+CREATE TABLE `daftar_produk` (
+  `id` int(11) NOT NULL,
+  `produk_id` int(11) NOT NULL,
+  `keranjang_id` char(36) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ekspedisi`
+--
 
 CREATE TABLE `ekspedisi` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nama` varchar(128) NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+  `status` enum('aktif','nonaktif') NOT NULL DEFAULT 'aktif',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `jenis_kelamin` enum('laki-laki','perempuan') NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` text NOT NULL,
+  `no_wa` varchar(15) NOT NULL,
+  `foto` varchar(45) NOT NULL,
+  `gaji` int(11) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` enum('aktif','nonaktif') NOT NULL DEFAULT 'aktif',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id` char(36) NOT NULL,
+  `judul` varchar(128) NOT NULL,
+  `akun_id` char(36) NOT NULL,
+  `pembeli_id` int(11) NOT NULL,
+  `status` enum('aktif','nonaktif') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lembur`
+--
+
+CREATE TABLE `lembur` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(128) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_wa` varchar(15) NOT NULL,
+  `email` varchar(364) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengiriman`
+--
 
 CREATE TABLE `pengiriman` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nama_kurir` varchar(128) NOT NULL,
   `no_wa_kurir` varchar(15) NOT NULL,
   `foto_kurir` varchar(45) NOT NULL,
-  `expedisi_id` int NOT NULL,
-  `wilayah_id` int NOT NULL,
-  `alamat` text NOT NULL,
-  `jumlah` int NOT NULL,
-  `foto_bukti` varchar(45) NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+  `ekspedisi_id` int(11) NOT NULL,
+  `biaya_kirim_id` int(11) NOT NULL,
+  `alamat_tujuan` text NOT NULL,
+  `jumlah_pengiriman` int(11) NOT NULL,
+  `foto_bukti` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
 
 CREATE TABLE `pesanan` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `tanggal` date NOT NULL,
-  `keranjang_id` int NOT NULL,
-  `nota` char(32) DEFAULT '',
-  `laporan` char(32) DEFAULT '',
-  `bukti_pelunasan` char(45) DEFAULT '',
-  `pengiriman_id` int,
-  `status` ENUM ('diperiksa', 'diterima', 'ditolak', 'diproses', 'dikirim', 'selesai') NOT NULL DEFAULT 'diperiksa',
-  `alasan` text,
-  `jenis_transaksi` ENUM ('langsung', 'payment gateway') NOT NULL DEFAULT 'langsung',
-  `transaksi_id` char(36) DEFAULT '',
-  `pendapatan_kotor` int NOT NULL,
-  `pendapatan_bersih` int NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+  `id` char(36) NOT NULL,
+  `keranjang_id` char(36) NOT NULL,
+  `pengiriman_id` int(11) DEFAULT NULL,
+  `jenis_transaksi` enum('langsung','payment gateway') NOT NULL DEFAULT 'langsung',
+  `id_transaksi` text DEFAULT NULL,
+  `pendapatan` bigint(20) DEFAULT NULL,
+  `biaya_sales` bigint(20) NOT NULL,
+  `biaya_pengiriman` bigint(20) DEFAULT NULL,
+  `kode_invoice` char(6) NOT NULL,
+  `bukti_pelunasan` varchar(45) DEFAULT NULL,
+  `nota_pembeli` char(32) DEFAULT NULL,
+  `nota_kurir` char(32) DEFAULT NULL,
+  `laporan_sales` char(32) DEFAULT NULL,
+  `laporan_internal` char(32) DEFAULT NULL,
+  `status` enum('diperiksa','diterima','ditolak','diproses','dikirim','selesai') NOT NULL DEFAULT 'diperiksa',
+  `alasan_ditolak` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `karyawan` (
-  `id` char(36) PRIMARY KEY,
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(11) NOT NULL,
   `nama` varchar(128) NOT NULL,
-  `jenis_kelamin` ENUM ('laki-laki', 'perempuan') NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `alamat` text NOT NULL,
-  `no_wa` varchar(15) UNIQUE NOT NULL,
-  `foto` char(45) UNIQUE NOT NULL,
-  `gaji` int NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+  `foto` char(32) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `isi` int(11) NOT NULL,
+  `satuan` varchar(64) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `biaya_sales` int(11) NOT NULL,
+  `stok_id` int(11) NOT NULL,
+  `status` enum('aktif','nonaktif') NOT NULL DEFAULT 'aktif',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `daftar_karyawan` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `karyawan_id` char(36) NOT NULL,
-  `gaji_tambahan` int NOT NULL,
-  `lembur_id` int NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `lembur` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `tanggal` date NOT NULL,
-  `judul` varchar(128) NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP
-);
+--
+-- Struktur dari tabel `stok`
+--
 
-ALTER TABLE `barang` ADD FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`id`);
+CREATE TABLE `stok` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `jumlah` bigint(20) NOT NULL,
+  `satuan` varchar(64) NOT NULL,
+  `status` enum('aktif','nonaktif') NOT NULL DEFAULT 'aktif',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `daftar_barang` ADD FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id`);
+--
+-- Indexes for dumped tables
+--
 
-ALTER TABLE `daftar_barang` ADD FOREIGN KEY (`keranjang_id`) REFERENCES `keranjang` (`id`);
+--
+-- Indeks untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `no_wa` (`no_wa`);
 
-ALTER TABLE `keranjang` ADD FOREIGN KEY (`akun_id`) REFERENCES `akun` (`id`);
+--
+-- Indeks untuk tabel `biaya_kirim`
+--
+ALTER TABLE `biaya_kirim`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `keranjang` ADD FOREIGN KEY (`pembeli_id`) REFERENCES `pembeli` (`id`);
+--
+-- Indeks untuk tabel `daftar_karyawan`
+--
+ALTER TABLE `daftar_karyawan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `karyawan_id` (`karyawan_id`),
+  ADD KEY `lembur_id` (`lembur_id`);
 
-ALTER TABLE `pengiriman` ADD FOREIGN KEY (`expedisi_id`) REFERENCES `ekspedisi` (`id`);
+--
+-- Indeks untuk tabel `daftar_produk`
+--
+ALTER TABLE `daftar_produk`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `produk_id` (`produk_id`),
+  ADD KEY `keranjang_id` (`keranjang_id`);
 
-ALTER TABLE `pengiriman` ADD FOREIGN KEY (`wilayah_id`) REFERENCES `biaya_pengiriman` (`id`);
+--
+-- Indeks untuk tabel `ekspedisi`
+--
+ALTER TABLE `ekspedisi`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `daftar_karyawan` ADD FOREIGN KEY (`karyawan_id`) REFERENCES `karyawan` (`id`);
+--
+-- Indeks untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `daftar_karyawan` ADD FOREIGN KEY (`lembur_id`) REFERENCES `lembur` (`id`);
+--
+-- Indeks untuk tabel `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `akun_id` (`akun_id`),
+  ADD KEY `pembeli_id` (`pembeli_id`);
 
-ALTER TABLE `pesanan` ADD FOREIGN KEY (`pengiriman_id`) REFERENCES `pengiriman` (`id`);
+--
+-- Indeks untuk tabel `lembur`
+--
+ALTER TABLE `lembur`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `pesanan` ADD FOREIGN KEY (`keranjang_id`) REFERENCES `pesanan` (`id`);
+--
+-- Indeks untuk tabel `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `pembeli` ADD FULLTEXT KEY `nama` (`nama`);
+
+--
+-- Indeks untuk tabel `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ekspedisi_id` (`ekspedisi_id`),
+  ADD KEY `biaya_kirim_id` (`biaya_kirim_id`);
+
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `keranjang_id` (`keranjang_id`),
+  ADD KEY `pengiriman_id` (`pengiriman_id`);
+
+--
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `stok_id` (`stok_id`);
+ALTER TABLE `produk` ADD FULLTEXT KEY `nama` (`nama`,`deskripsi`);
+
+--
+-- Indeks untuk tabel `stok`
+--
+ALTER TABLE `stok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `biaya_kirim`
+--
+ALTER TABLE `biaya_kirim`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `daftar_karyawan`
+--
+ALTER TABLE `daftar_karyawan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `daftar_produk`
+--
+ALTER TABLE `daftar_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `ekspedisi`
+--
+ALTER TABLE `ekspedisi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `lembur`
+--
+ALTER TABLE `lembur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `stok`
+--
+ALTER TABLE `stok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `daftar_karyawan`
+--
+ALTER TABLE `daftar_karyawan`
+  ADD CONSTRAINT `daftar_karyawan_ibfk_1` FOREIGN KEY (`karyawan_id`) REFERENCES `karyawan` (`id`),
+  ADD CONSTRAINT `daftar_karyawan_ibfk_2` FOREIGN KEY (`lembur_id`) REFERENCES `lembur` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `daftar_produk`
+--
+ALTER TABLE `daftar_produk`
+  ADD CONSTRAINT `daftar_produk_ibfk_1` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`),
+  ADD CONSTRAINT `daftar_produk_ibfk_2` FOREIGN KEY (`keranjang_id`) REFERENCES `keranjang` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD CONSTRAINT `keranjang_ibfk_1` FOREIGN KEY (`akun_id`) REFERENCES `akun` (`id`),
+  ADD CONSTRAINT `keranjang_ibfk_2` FOREIGN KEY (`pembeli_id`) REFERENCES `pembeli` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  ADD CONSTRAINT `pengiriman_ibfk_1` FOREIGN KEY (`ekspedisi_id`) REFERENCES `ekspedisi` (`id`),
+  ADD CONSTRAINT `pengiriman_ibfk_2` FOREIGN KEY (`biaya_kirim_id`) REFERENCES `biaya_kirim` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`keranjang_id`) REFERENCES `keranjang` (`id`),
+  ADD CONSTRAINT `pesanan_ibfk_2` FOREIGN KEY (`pengiriman_id`) REFERENCES `pengiriman` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`stok_id`) REFERENCES `stok` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
