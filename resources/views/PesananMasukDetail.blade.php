65ad9ea7803649e859e2aaa176ac96da9c0922cd
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-04-06 10:40:09 +0700

    Redesain kop

--- resources/views/PesananMasukDetail.blade.php
+++ resources/views/PesananMasukDetail.blade.php
@@ -289,7 +289,7 @@
 
                     <div class="mb-4">
                         <label class="block text-sm font-medium text-gray-700 mb-1" for="alamat_tujuan">Alamat Tujuan</label>
-                        <textarea name="alamat_tujuan" id="alamat_tujuan" cols="30" rows="8" style="resize: none;" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required></textarea>
+                        <textarea name="alamat_tujuan" id="alamat_tujuan" cols="30" rows="8" style="resize: none;" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400" required>{{$pembeli->alamat}}</textarea>
                     </div>
                 </div>
             </div>
